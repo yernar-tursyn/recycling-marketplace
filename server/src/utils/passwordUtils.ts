5fa8ebbf7efb7d87d89e5738f5501d96f7c90538
Kanat Juzbayev <kanat.juzbayev@gmail.com>
2025-04-11 11:11:46 +0500

    fix: generateToken fix

--- server/src/utils/passwordUtils.ts
+++ server/src/utils/passwordUtils.ts
@@ -14,7 +14,11 @@ export const comparePasswords = async (
 };
 
 export const generateToken = (userId: number): string => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET || "your-secret-key", {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined in environment variables");
+  }
+
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
 };
