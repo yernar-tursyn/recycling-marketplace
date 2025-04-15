391a9a5416d24acf05a82b036687ab971bfa7a55
Kanat Juzbayev <kanat.juzbayev@gmail.com>
2025-04-15 09:43:19 +0500

    fix: consist NEXT_PUBLIC_API_URL

--- client/services/api.ts
+++ client/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
