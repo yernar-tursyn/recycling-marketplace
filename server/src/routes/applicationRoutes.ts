cd6c08072df00ce34a4c3b12a49129a9842129a8
Kanat Juzbayev <kanat.juzbayev@gmail.com>
2025-04-15 15:40:11 +0500

    fix: sever eror

--- server/src/routes/applicationRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getAllApplications } from "../controllers/applicationController";
-
-const router = Router();
-
-router.get("/", getAllApplications);
-
-export default router;
