0d1a9f4ed0e461f952ce1fd121ff28bb5f717cd9
Kanat Juzbayev <kanat.juzbayev@gmail.com>
2025-04-17 08:55:04 +0500

    fix: add material categories const

--- client/app/marketplace/page.tsx
+++ client/app/marketplace/page.tsx
@@ -27,17 +27,10 @@ import { Heart, Search } from "lucide-react";
 import { useFavorites } from "@/context/favorites-context";
 import type { MaterialType } from "@/types/material";
 
-export const MATERIAL_CATEGORIES = {
-  1: "Пластик",
-  2: "Макулатура",
-  3: "Стекло",
-  4: "Металл",
-  5: "Кожа/резина/ветошь",
-  6: "Органика",
-  7: "Отходы",
-} as const;
-
-export type MaterialCategory = keyof typeof MATERIAL_CATEGORIES;
+import {
+  MATERIAL_CATEGORIES,
+  MaterialCategory,
+} from "@/types/constants/materialCategories";
 
 export default function MarketplacePage() {
   const [materials, setMaterials] = useState<MaterialType[]>([]);
